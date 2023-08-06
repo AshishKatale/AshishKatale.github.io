8b1de211c598c109f8140f726771591bea2180f4
Ashish Katale <61214878+AshishKatale@users.noreply.github.com>
2023-08-06 09:48:05 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { createSignal } from 'solid-js'
+import './App.css'
+
+function App() {
+  const [count, setCount] = createSignal(0)
+
+  return (
+    <>
+      <button onClick={() => setCount(count() + 1)}>
+        {count()}
+      </button>
+    </>
+  )
+}
+
+export default App
